534aad0c73b65055a3c37829d6784ee1e230c553
Zeyu Shen <zs7353@della-gpu.princeton.edu>
2024-11-29 07:30:33 -0500

    more experiments

--- scripts/MATH2/bfs.sh
+++ scripts/MATH2/bfs.sh
@@ -1,10 +1,10 @@
 python3.11 -B run.py \
-    --backend gpt-4o \
+    --backend Claude-3.5-Sonnet \
     --task MATH2 \
-    --task_start_index 0 \
+    --task_start_index 80 \
     --task_end_index 100 \
     --method_select greedy \
-    --apply_skills \
+    --decompose_problem \
     --n_generate_sample 3 \
     --n_evaluate_sample 2 \
     --n_select_sample 2 \
